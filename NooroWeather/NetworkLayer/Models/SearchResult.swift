3a0de9ace0fed418c5a0758c032453a4a7a2a4a3
Joey Nash <jrnash621@gmail.com>
2024-12-17 14:59:34 -0500

    Prototype homepage

---
+++ NooroWeather/NetworkLayer/Models/SearchResult.swift
@@ -0,0 +1,8 @@
+//
+//  SearchResult.swift
+//  NooroWeather
+//
+//  Created by Joseph Nash on 12/17/24.
+//
+
+import Foundation
---
+++ NooroWeather/NetworkLayer/Service/UserDefaultsService.swift
@@ -0,0 +1,8 @@
+//
+//  UserDefaultsService.swift
+//  NooroWeather
+//
+//  Created by Joseph Nash on 12/17/24.
+//
+
+import Foundation
